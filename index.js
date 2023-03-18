056c31167cc378202257238cdd568c71bf1d2ebf
andy <andyugbawa@gmail.com>
2023-03-19 00:15:53 +0100

    completed edit button

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const checkButton= document.querySelector('.check-btn');
 const header = document.querySelector('.ink');
 const updateButton = document.querySelector('.update-btn');
-const deleteButton =document.querySelector('.delete-btn')
 const completeButton = document.querySelector('.check-btn')
 const todoInput = document.querySelector('.todo-input');
+const saveButton = document.querySelector('.save-btn');
 const game =document.getElementById('game');
 checkButton.addEventListener('click',createTodo);
 
@@ -40,7 +40,7 @@ function createTodo(){
 }
 
 function cancelButton(e){
-    e.target.parentElement.remove()
+    e.target.parentElement.remove();
 }
 
 function update(e){ 
@@ -51,8 +51,6 @@ function update(e){
         e.target.parentElement.firstChild.classList.add('strike');
     }
 }
-
-
 
 function editButton(e){
     let editElement = e.target.parentElement.children;
@@ -74,8 +72,35 @@ function editButton(e){
 
 function saveBtn(e){
     e.preventDefault()
-   console.log('something')
+
+    const newDiv = document.createElement('div')
+    const newList =document.createElement('li');
+    newList.classList.add('bug')
+    newDiv.appendChild(newList)
+    game.classList.add('case') 
+ 
+    newList.innerText=e.target.parentElement.children[0].value;
+    completeButton.innerText='Edit';
+    completeButton.classList.add('check-btn')
+    newDiv.appendChild(completeButton)
+    completeButton.addEventListener('click',editButton);
+    game.appendChild(newDiv)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText='Delete';
+    deleteButton.classList.add('delete-btn')
+    deleteButton.addEventListener('click',cancelButton)
+    newDiv.appendChild(deleteButton)
+    game.appendChild(newDiv)
+
+    const updateButton = document.createElement('button');
+    updateButton.id=todoInput.value;
+    updateButton.innerText='Update';
+    updateButton.classList.add('update-btn');
+    updateButton.addEventListener('click',update)
+    newDiv.appendChild(updateButton);
+    game.appendChild(newDiv);
+
+    e.target.parentElement.children[0].remove();
+    e.target.remove();
 }
-
-
-
