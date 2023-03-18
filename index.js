a0a7b7c76381103276612eb80026171509c62e6e
manosakpujiha <manos.akpujiha@gmail.com>
2023-03-17 21:08:16 -0400

    fixed prevent default

--- index.js
+++ index.js
@@ -1,153 +1,80 @@
-
-
 const checkButton= document.querySelector('.check-btn');
 const header = document.querySelector('.ink');
 const updateButton = document.querySelector('.update-btn');
 const deleteButton =document.querySelector('.delete-btn')
 const completeButton = document.querySelector('.check-btn')
-
 const todoInput = document.querySelector('.todo-input');
 const game =document.getElementById('game');
-
-//manos edited this
-
-// andy edited also =
-
 checkButton.addEventListener('click',createTodo);
-
-
-
-
-
-
 
 function createTodo(){
     const newDiv = document.createElement('div')
     const newList =document.createElement('li');
     newList.classList.add('bug')
-    // newList.id=todoInput.value;
     newDiv.appendChild(newList)
     game.classList.add('case') 
     newList.innerText=todoInput.value;
-    
 
-
-
-
- 
-  
-
-   const completeButton = document.createElement('button')
+    const completeButton = document.createElement('button')
     completeButton.innerText='Edit';
     completeButton.classList.add('check-btn')
     newDiv.appendChild(completeButton)
     completeButton.addEventListener('click',editButton);
     game.appendChild(newDiv)
 
-   const deleteButton = document.createElement('button')
+    const deleteButton = document.createElement('button')
     deleteButton.innerText='Delete';
     deleteButton.classList.add('delete-btn')
     deleteButton.addEventListener('click',cancelButton)
     newDiv.appendChild(deleteButton)
     game.appendChild(newDiv)
 
-   const updateButton = document.createElement('button');
-   updateButton.id=todoInput.value;
+    const updateButton = document.createElement('button');
+    updateButton.id=todoInput.value;
     updateButton.innerText='Update';
     updateButton.classList.add('update-btn');
     updateButton.addEventListener('click',update)
-
     newDiv.appendChild(updateButton);
     game.appendChild(newDiv);
-
-
-
-    
-
-    
     todoInput.value='';
-     
 }
 
 function cancelButton(e){
     e.target.parentElement.remove()
-
-    // e.target.parentElement.children[0].remove()
-    // console.log(e.target)
-
-    // e.target.parentElement.remove();
-    // const newList = e.target;
-    // newList.classList.add('andy')
-    // newList.classList.add('manos')
-    // console.log('1',newList,'2',newList.classList,'3',newList.classList['0'])
-    
-    
-    // if (newList.classList[0] === 'delete-btn'){
-        
-    //     const list = newList.parentElement;
-    //     list.remove();
-    // }
-   
-
-    
 }
 
-function update(e){
-    // console.log(e.target.parentElement.firstChild);
-    
+function update(e){ 
     if (e.target.parentElement.firstChild.classList.contains('strike')){
         e.target.parentElement.firstChild.classList.remove('strike');
     }
     else{
         e.target.parentElement.firstChild.classList.add('strike');
     }
-    
-
-    
-    
-    
 }
-let saveButton2 = document.createElement('button')
-saveButton2.addEventListener('click',saveBtn2);
+
+
+
 function editButton(e){
     let editElement = e.target.parentElement.children;
     editElement[0].remove();
     editElement[0].remove();
     editElement[0].remove();
     editElement[0].remove();
-
-   
-    
-  
     const inputElement = document.createElement('input')
     const newForm = document.createElement('form')
     newForm.appendChild(inputElement)
-    
-  
- 
-    saveButton2.innerText='save';
-    saveButton2.classList.add('save-btn')
+    let saveButton = document.createElement('button')
+    saveButton.addEventListener('click', saveBtn);
+    saveButton.innerText='save';
+    saveButton.classList.add('save-btn')
     inputElement.setAttribute('placeholder','Edit...')
-    newForm.appendChild(saveButton2);
+    newForm.appendChild(saveButton);
     game.appendChild(newForm);
-    
-    ///manos edited
-    
-    
-
-
-
-    
-
 }
 
-
-
-
-function saveBtn2(e){
-    console.log('smmt')
-
-    
+function saveBtn(e){
+    e.preventDefault()
+   console.log('something')
 }
 
 
